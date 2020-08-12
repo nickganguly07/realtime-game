d329974c287f3747f6d187f653061771f68ab209
Bartłomiej Fryz <fryzbartlomiej@gmail.com>
2020-08-13 01:44:41 +0530

    Added build

---
+++ build/precache-manifest.0cc131e2a1dcebfef6dd4495a8094186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6632acbb5dcbf0801f5ce45bb3d8489a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c47cc4c03bc7be47aeb3",
+    "url": "/static/css/main.de230cdc.chunk.css"
+  },
+  {
+    "revision": "0d7f12921bc15fc5dd8f",
+    "url": "/static/js/2.83402fba.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.83402fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c47cc4c03bc7be47aeb3",
+    "url": "/static/js/main.8c42eef5.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  }
+]);
